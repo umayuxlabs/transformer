ee76a9aac6a3879800601d49bb9b98ecebd6b13a
Gustavo Arango <gaarangoa@gmail.com>
2019-04-13 14:51:14 -0400

    added training script

--- train.py
+++ train.py
@@ -1,74 +1,120 @@
 import tensorflow_datasets as tfds
 import tensorflow as tf
-
-
-examples, metadata = tfds.load(
-    "ted_hrlr_translate/pt_to_en", with_info=True, as_supervised=True
-)
-
-train_examples, val_examples = examples["train"], examples["validation"]
-
-tokenizer_en = tfds.features.text.SubwordTextEncoder.build_from_corpus(
-    (en.numpy() for pt, en in train_examples), target_vocab_size=2 ** 13
-)
-
-tokenizer_pt = tfds.features.text.SubwordTextEncoder.build_from_corpus(
-    (pt.numpy() for pt, en in train_examples), target_vocab_size=2 ** 13
-)
-
-
-def encode(lang1, lang2):
-    """
-        Add a start and end token to the input and target.
-    """
-    lang1 = (
-        [tokenizer_pt.vocab_size]
-        + tokenizer_pt.encode(lang1.numpy())
-        + [tokenizer_pt.vocab_size + 1]
-    )
-
-    lang2 = (
-        [tokenizer_en.vocab_size]
-        + tokenizer_en.encode(lang2.numpy())
-        + [tokenizer_en.vocab_size + 1]
-    )
-
-    return lang1, lang2
-
-
-def filter_max_length(x, y, max_length=MAX_LENGTH):
-    return tf.logical_and(tf.size(x) <= max_length, tf.size(y) <= max_length)
-
-
-def tf_encode(pt, en):
-    return tf.py_function(encode, [pt, en], [tf.int64, tf.int64])
-
+import src.dataset as dt
+from src.optimizer import CustomSchedule, loss_function
+from src.model import Transformer
+import time
+from src.masking import create_masks
 
 MAX_LENGTH = 40
 BUFFER_SIZE = 20000
 BATCH_SIZE = 64
+EPOCHS = 2
 num_layers = 4
 d_model = 128
 dff = 512
 num_heads = 8
 dropout_rate = 0.1
+test_partition = 0.2
+dataset_file = "./data/test.tsv"
+checkpoint_path = "./data/checkpoints/train"
 
-train_dataset = train_examples.map(tf_encode)
-train_dataset = train_dataset.filter(filter_max_length)
-# cache the dataset to memory to get a speedup while reading from it.
+
+# Build the dataset for training validation
+dataset = dt.Dataset(filename=dataset_file)
+dataset.build_train_test(test=test_partition)
+train_examples, val_examples = dataset.format_train_test()
+tokenizer_source, tokenizer_target = dataset.tokenizer(train_examples)
+
+train_dataset = train_examples.map(dataset.tf_encode)
+train_dataset = train_dataset.filter(dataset.filter_max_length)
 train_dataset = train_dataset.cache()
 train_dataset = train_dataset.shuffle(BUFFER_SIZE).padded_batch(
     BATCH_SIZE, padded_shapes=([-1], [-1])
 )
 train_dataset = train_dataset.prefetch(tf.data.experimental.AUTOTUNE)
 
-
-val_dataset = val_examples.map(tf_encode)
-val_dataset = val_dataset.filter(filter_max_length).padded_batch(
+val_dataset = val_examples.map(dataset.tf_encode)
+val_dataset = val_dataset.filter(dataset.filter_max_length).padded_batch(
     BATCH_SIZE, padded_shapes=([-1], [-1])
 )
 
+input_vocab_size = tokenizer_source.vocab_size + 2
+target_vocab_size = tokenizer_target.vocab_size + 2
 
-input_vocab_size = tokenizer_pt.vocab_size + 2
-target_vocab_size = tokenizer_en.vocab_size + 2
+# Setup the learning rate and optimizer
+learning_rate = CustomSchedule(d_model)
+optimizer = tf.keras.optimizers.Adam(
+    learning_rate, beta_1=0.9, beta_2=0.98, epsilon=1e-9
+)
+
+train_loss = tf.keras.metrics.Mean(name="train_loss")
+train_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name="train_accuracy")
+
+transformer = Transformer(
+    num_layers,
+    d_model,
+    num_heads,
+    dff,
+    input_vocab_size,
+    target_vocab_size,
+    dropout_rate,
+)
+
+ckpt = tf.train.Checkpoint(transformer=transformer, optimizer=optimizer)
+ckpt_manager = tf.train.CheckpointManager(ckpt, checkpoint_path, max_to_keep=5)
+
+# if a checkpoint exists, restore the latest checkpoint.
+if ckpt_manager.latest_checkpoint:
+    ckpt.restore(ckpt_manager.latest_checkpoint)
+    print("Latest checkpoint restored!!")
+
+
+@tf.function
+def train_step(inp, tar):
+    tar_inp = tar[:, :-1]
+    tar_real = tar[:, 1:]
+
+    enc_padding_mask, combined_mask, dec_padding_mask = create_masks(inp, tar_inp)
+
+    with tf.GradientTape() as tape:
+        predictions, _ = transformer(
+            inp, tar_inp, True, enc_padding_mask, combined_mask, dec_padding_mask
+        )
+        loss = loss_function(tar_real, predictions)
+
+    gradients = tape.gradient(loss, transformer.trainable_variables)
+    optimizer.apply_gradients(zip(gradients, transformer.trainable_variables))
+
+    train_loss(loss)
+    train_accuracy(tar_real, predictions)
+
+
+for epoch in range(EPOCHS):
+    start = time.time()
+
+    train_loss.reset_states()
+    train_accuracy.reset_states()
+
+    # inp -> portuguese, tar -> english
+    for (batch, (inp, tar)) in enumerate(train_dataset):
+        train_step(inp, tar)
+        if batch % 500 == 0:
+            print(
+                "Epoch {} Batch {} Loss {:.4f} Accuracy {:.4f}".format(
+                    epoch + 1, batch, train_loss.result(), train_accuracy.result()
+                )
+            )
+
+    if (epoch + 1) % 5 == 0:
+        ckpt_save_path = ckpt_manager.save()
+        print("Saving checkpoint for epoch {} at {}".format(epoch + 1, ckpt_save_path))
+
+    print(
+        "Epoch {} Loss {:.4f} Accuracy {:.4f}".format(
+            epoch + 1, train_loss.result(), train_accuracy.result()
+        )
+    )
+
+    print("Time taken for 1 epoch: {} secs\n".format(time.time() - start))
 
