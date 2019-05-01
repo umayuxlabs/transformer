fe513dcd86392e2b5a024c3a667bebf0ddef7820
Gustavo Arango <gaarangoa@gmail.com>
2019-04-30 21:49:39 -0400

    update train

--- utensor/train.py
+++ utensor/train.py
@@ -11,5 +11,5 @@ def get_parser():
     parser.add_argument("--weight_decay", default=0.01, type=float)
     parser.add_argument("--epochs", default=50, type=int)
     parser.add_argument("--max_patience", default=5, type=int)
-    parser.add_argument("--factor", default=0.3, type=float)
+
     return parser
